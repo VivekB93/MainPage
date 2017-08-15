6da93c906dd1127fd7c93347fff1bdc3080ab12a
Vivek Bhagwani <imeinstien2@gmail.com>
2017-08-16 04:23:15 +0530

    about, contact, projects added. Main updated

---
+++ js/myJS.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+// 	alert("Vivek");
+// 	$(window).on('resize', function () {
+// 	    if($(window).width() < 768) {
+// 	    	$('.row1-video').remove();
+// 	    	$('.row1-videobox').prepend($('.row1-video'));
+// 	    }
+// 	});
+// });
